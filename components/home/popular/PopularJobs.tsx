de1eb8cfc3a099a5f4f459a35a5fc73880856bca
OkkashaAlly <steamnature006@gmail.com>
2023-03-21 07:49:29 +0300

    INIT

---
+++ components/home/popular/PopularJobs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const PopularJobs = () => {
+  return (
+    <View>
+      <Text>PopularJobs</Text>
+    </View>
+  );
+};
+
+export default PopularJobs;
---
+++ components/index.ts
@@ -0,0 +1,29 @@
+import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
+
+// home screen
+import NearbyJobs from "./home/nearby/NearbyJobs";
+import PopularJobs from "./home/popular/PopularJobs";
+import Welcome from "./home/welcome/Welcome";
+
+// job details screen
+import { default as JobAbout } from "./jobDetails/about/About";
+import Company from "./jobDetails/company/Company";
+import { default as JobFooter } from "./jobDetails/footer/Footer";
+import Specifics from "./jobDetails/specifics/Specifics";
+import { default as JobTabs } from "./jobDetails/tabs/Tabs";
+
+// common
+import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
+
+export {
+  ScreenHeaderBtn,
+  Welcome,
+  NearbyJobs,
+  PopularJobs,
+  Company,
+  JobTabs,
+  JobAbout,
+  JobFooter,
+  Specifics,
+  NearbyJobCard,
+};
