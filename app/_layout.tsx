c1d39c2134ac020b2170c50be6c22b4e2e4638ac
OkkashaAlly <steamnature006@gmail.com>
2023-03-23 08:37:04 +0300

    COMPONENTS/HOME: Welcome.tsx - SearchBar extended component added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,32 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import React from "react";
 
+// SplashScreen.preventAutoHideAsync();
+
+// ================================================
+// LAYOUT COMPONENT ===============================
+// ================================================
 export default function () {
+  // load fonts
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  // // load font on app start
+  // const onLayoutRootView = React.useCallback(async () => {
+  //   try {
+  //     await SplashScreen.hideAsync();
+  //   } catch (e) {
+  //     console.warn(e);
+  //   }
+  // }, [fontsLoaded]);
+
+  // show only if fonts are loaded
+  if (!fontsLoaded) return null;
+
+  // RETURN ///////////////////////////////////////
   return <Stack />;
 }
--- components/home/welcome/Welcome.tsx
+++ components/home/welcome/Welcome.tsx
@@ -1,14 +1,49 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { Image, Text, TextInput, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { icons } from "../../../constants";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
 
-const Welcome = () => {
+// =========================================================
+// WELCOME PAGE COMPONENT ==================================
+// =========================================================
+export default function Welcome() {
   return (
     <View>
-      <Text>Welcome</Text>
+      {/* top  */}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Okkasha</Text>
+        <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text>
+      </View>
+      {/* search bar  */}
+      <SearchBar />
     </View>
-  )
+  );
 }
 
-export default Welcome
+// EXTENDED COMPONENT ///////////////////////////////
+const SearchBar = () => {
+  return (
+    <View style={styles.searchContainer}>
+      {/* input  */}
+      <View style={styles.searchWrapper}>
+        <TextInput
+          style={styles.searchInput}
+          value=""
+          placeholder="What are you looking for?"
+          onChangeText={() => {}}
+        />
+      </View>
+      {/* button  */}
+      <TouchableOpacity style={styles.searchBtn}>
+        <Image
+          source={icons.search}
+          resizeMode="contain"
+          // @ts-ignore
+          style={styles.searchBtnImage}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
